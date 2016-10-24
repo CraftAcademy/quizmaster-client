87daedafa53cdaff1ab4a430b8c8994972fd4017
Amber <amber@amberwilkie.com>
2016-10-24 13:55:17 +0200

    Change app name in index

--- www/js/app.js
+++ www/js/app.js
@@ -48,35 +48,6 @@ angular.module('quizmaster', ['ionic', 'quizmaster.controllers', 'quizmaster.ser
         controller: 'DashCtrl'
       }
     }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
   });
 
   // if none of the above states are matched, use this as the fallback
