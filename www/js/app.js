d819997db5f0588ceaab3820a1b2edc750e8f63d
Amber Wilkie <amber@amberwilkie.com>
2016-10-25 11:11:59 +0200

    Teams can see questions when they are on the mobile Quiz page (#2)
    * Install angular-actioncable
    * Change app name in index
    * Show quiz page modal
    * Show only modal on Quiz page
    * Insert subscription bits
    * Hopefully got the server configs right
    * The right line to make it receive data from the channel
    * Finally get messages to appear in view
    * Success - receiving data from mobile
    * Grab information out of cable partial into submitAnswer();
    * Add www/lib to .gitignore
    * Remove www/lib from version control

--- www/js/app.js
+++ www/js/app.js
@@ -7,79 +7,55 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('quizmaster', ['ionic', 'quizmaster.controllers', 'quizmaster.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
+  .run(function (ActionCableConfig) {
+    ActionCableConfig.wsUri = "ws://localhost:3000/cable";
+    ActionCableConfig.autoStart = true;
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'QuizController'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,64 @@
-angular.module('quizmaster.controllers', [])
+angular.module('quizmaster.controllers', [ 'ngActionCable' ])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope) {
+  })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('QuizController', function ($scope, $ionicModal, ActionCableChannel, $interpolate) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    $scope.goToQuiz = function () {
+      $ionicModal.fromTemplateUrl('templates/quiz-page.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function (modal) {
+        $scope.modal = modal;
+        $scope.openModal();
+      });
+    };
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    var consumer = new ActionCableChannel('QuizChannel');
+    var callback = function(data) {
+      console.log(data);
+      if(data.welcome == 'true'){
+        angular.element(document.querySelector('#message')).html(data.message);
+      } else {
+        angular.element(document.querySelector('#message')).html(data);
+        this.submitAnswer = function() {
+          var dataset, quiz, answer, question, team ;
+          dataset = angular.element(document.querySelector('#info'))[0];
+          quiz = dataset.getAttribute('data-quiz-id');
+          // team = dataset.getAttribute('data-team-id');
+          question = dataset.getAttribute('data-question-id');
+          answer = $this.find('#body');
+          if ($.trim(answer.val()).length >= 1) {
+            // Team ID needs to come from cookies or something here.
+            // App.quiz.submitAnswer({answer: answer.val(), team_id: team, quiz_id: quiz, question_id: question});
+            console.log('inside if of submitAnswer');
+            $('.answer_form').hide();
+            $('.wait').show();
+          } else {
+            $('#message').append('WTF Dude?!?')
+          }
+          return false;
+        };
+      }
+    };
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+
+
+    $scope.openModal = function () {
+      $scope.modal.show();
+      consumer.subscribe(callback)
+        .then(function(data) {
+        });
+
+    }
+
+  });
+
