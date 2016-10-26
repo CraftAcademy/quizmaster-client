3df7a72cca247d3926bd8684861b52415dd564af
Amber Wilkie <amber@amberwilkie.com>
2016-10-26 17:02:46 +0200

    Teams can enter their answers and have them saved on the server (#3)
    * submitAnswer inputs are what the server expects (minus team id)
    
    * Configure submitAnswer() to send something close to useful to server
    
    * Use correct Ruby method in .send
    
    * Add in CSS from server
    
    * Take out server's CSS
    
    * Add quiz_id to subscribe to the right channel

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,40 +22,44 @@ angular.module('quizmaster.controllers', [ 'ngActionCable' ])
       });
     };
 
-    var consumer = new ActionCableChannel('QuizChannel');
+    var consumer = new ActionCableChannel('QuizChannel', {quiz_id: 43, team_id: 2});
     var callback = function(data) {
       console.log(data);
       if(data.welcome == 'true'){
         angular.element(document.querySelector('#message')).html(data.message);
       } else {
         angular.element(document.querySelector('#message')).html(data);
-        this.submitAnswer = function() {
-          var dataset, quiz, answer, question, team ;
-          dataset = angular.element(document.querySelector('#info'))[0];
-          quiz = dataset.getAttribute('data-quiz-id');
-          // team = dataset.getAttribute('data-team-id');
-          question = dataset.getAttribute('data-question-id');
-          answer = $this.find('#body');
-          if ($.trim(answer.val()).length >= 1) {
-            // Team ID needs to come from cookies or something here.
-            // App.quiz.submitAnswer({answer: answer.val(), team_id: team, quiz_id: quiz, question_id: question});
-            console.log('inside if of submitAnswer');
-            $('.answer_form').hide();
-            $('.wait').show();
-          } else {
-            $('#message').append('WTF Dude?!?')
-          }
-          return false;
-        };
       }
     };
-
-
 
     $scope.openModal = function () {
       $scope.modal.show();
       consumer.subscribe(callback)
-        .then(function(data) {
+        .then(function() {
+          this.submitAnswer = function() {
+            var dataset, quiz, answer, question, team, answer_hash ;
+            dataset = angular.element(document.querySelector('#info'))[0];
+            quiz = dataset.getAttribute('data-quiz-id');
+            // team = dataset.getAttribute('data-team-id');
+            team = 2;
+            question = dataset.getAttribute('data-question-id');
+            answer = angular.element(document.querySelector('#body'))[0].value;
+
+            var sendAnswer = function() {
+              consumer.send(answer_hash, 'submit_answer');
+            };
+
+            if (answer.trim().length >= 1) {
+              // Team ID needs to come from cookies or something here. - hard-coded as 1 for the moment
+              answer_hash = {answer: answer, team_id: team, quiz_id: quiz, question_id: question};
+              sendAnswer(answer_hash);
+              // $('.answer_form').hide();
+              // $('.wait').show();
+            } else {
+              angular.element(document.querySelector('#message')).html('Enter an answer!');
+            }
+            return false;
+          };
         });
 
     }
