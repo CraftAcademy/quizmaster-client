cf90f1106062fd17ce63e0cc936eac932151284d
thesuss <lullisuss@gmail.com>
2016-10-26 08:26:48 +0200

    removed the tabs

--- www/js/app.js
+++ www/js/app.js
@@ -28,7 +28,7 @@ angular.module('quizmaster', ['ionic', 'quizmaster.controllers', 'quizmaster.ser
     ActionCableConfig.autoStart = true;
   })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($urlRouterProvider, $stateProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
@@ -36,26 +36,13 @@ angular.module('quizmaster', ['ionic', 'quizmaster.controllers', 'quizmaster.ser
     // Each state's controller can be found in controllers.js
     $stateProvider
 
-    // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-      })
-
-      // Each tab has its own nav history stack:
-
-      .state('tab.dash', {
-        url: '/dash',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/tab-dash.html',
-            controller: 'QuizController'
-          }
-        }
+      .state('tabs', {
+        url: '/tabs',
+        templateUrl: 'templates/tabs.html',
+        controller:"QuizController"
       });
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
+    $urlRouterProvider.otherwise('/tabs');
 
   });
