4f932de785ae5b82ad7a1124f198c9abd983ea8a
Amber <amber@amberwilkie.com>
2016-10-26 18:07:11 +0200

    Get quiz from server, use to subscribe to right channel

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,9 @@
-angular.module('quizmaster.controllers', [ 'ngActionCable' ])
+angular.module('quizmaster.controllers', ['ngActionCable'])
 
   .controller('DashCtrl', function ($scope) {
   })
 
-  .controller('QuizController', function ($scope, $ionicModal, ActionCableChannel, $interpolate) {
+  .controller('QuizController', function ($scope, $ionicModal, ActionCableChannel) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -11,61 +11,88 @@ angular.module('quizmaster.controllers', [ 'ngActionCable' ])
     //
     //$scope.$on('$ionicView.enter', function(e) {
     //});
-    var consumer, callback;
+    var consumer, callback, quiz;
 
-    $scope.findQuiz = function (code) {
+    $scope.findQuiz = function () {
+      code = angular.element(document.querySelector('#codeEntry'))[0].value;
       consumer = new ActionCableChannel('QuizChannel');
+      callback = function (obj) {
+        console.log(obj);
+        quiz = obj;
+        consumer.unsubscribe()
+          .then(function () {
+            console.log('unsubscribed');
+            subscribeToQuiz();
+          });
+      };
+      consumer.subscribe(callback)
+        .then(function () {
+          consumer.send(code, 'get_quiz');
+        });
 
       $ionicModal.fromTemplateUrl('templates/quiz-page.html', {
         scope: $scope,
         animation: 'slide-in-up'
       }).then(function (modal) {
         $scope.modal = modal;
+        $scope.quiz = quiz;
         $scope.openModal();
       });
     };
 
-    consumer = new ActionCableChannel('QuizChannel', {quiz_id: 43, team_id: 2});
-    callback = function(data) {
-      console.log(data);
-      if(data.welcome == 'true'){
-        angular.element(document.querySelector('#message')).html(data.message);
-      } else {
-        angular.element(document.querySelector('#message')).html(data);
-      }
+    var subscribeToQuiz = function () {
+      callback = function (data) {
+        console.log(data);
+        if (data.welcome == 'true') {
+          angular.element(document.querySelector('#message')).html(data.message);
+        } else {
+          angular.element(document.querySelector('#message')).html(data);
+        }
+      };
+      consumer = new ActionCableChannel('QuizChannel', {quiz_id: quiz.id});
+      consumer.subscribe(callback)
+        .then(function () {
+          subscribeToQuizChannel();
+        });
+
+
     };
+
+    var subscribeToQuizChannel = function () {
+        this.submitAnswer = function () {
+          var dataset, quiz, answer, question, team, answer_hash;
+          dataset = angular.element(document.querySelector('#info'))[0];
+          quiz = dataset.getAttribute('data-quiz-id');
+          // team = dataset.getAttribute('data-team-id');
+          team = 2;
+          question = dataset.getAttribute('data-question-id');
+          answer = angular.element(document.querySelector('#body'))[0].value;
+
+          var sendAnswer = function () {
+            consumer.send(answer_hash, 'submit_answer');
+          };
+
+          if (answer.trim().length >= 1) {
+            // Team ID needs to come from cookies or something here. - hard-coded as 1 for the moment
+            answer_hash = {answer: answer, team_id: team, quiz_id: quiz, question_id: question};
+            sendAnswer(answer_hash);
+            // $('.answer_form').hide();
+            // $('.wait').show();
+          } else {
+            angular.element(document.querySelector('#message')).html('Enter an answer!');
+          }
+          return false;
+        };
+        this.registerTeam = function () {
+          console.log('registerTeam function');
+        };
+      }
+      ;
 
     $scope.openModal = function () {
       $scope.modal.show();
-      consumer.subscribe(callback)
-        .then(function() {
-          this.submitAnswer = function() {
-            var dataset, quiz, answer, question, team, answer_hash ;
-            dataset = angular.element(document.querySelector('#info'))[0];
-            quiz = dataset.getAttribute('data-quiz-id');
-            // team = dataset.getAttribute('data-team-id');
-            team = 2;
-            question = dataset.getAttribute('data-question-id');
-            answer = angular.element(document.querySelector('#body'))[0].value;
-
-            var sendAnswer = function() {
-              consumer.send(answer_hash, 'submit_answer');
-            };
-
-            if (answer.trim().length >= 1) {
-              // Team ID needs to come from cookies or something here. - hard-coded as 1 for the moment
-              answer_hash = {answer: answer, team_id: team, quiz_id: quiz, question_id: question};
-              sendAnswer(answer_hash);
-              // $('.answer_form').hide();
-              // $('.wait').show();
-            } else {
-              angular.element(document.querySelector('#message')).html('Enter an answer!');
-            }
-            return false;
-          };
-        });
-
     }
 
-  });
+  })
+;
 
