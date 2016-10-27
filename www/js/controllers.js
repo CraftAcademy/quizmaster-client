2f97819fd2decea4b27f4182a4a742921dfedc2a
Amber Wilkie <amber@amberwilkie.com>
2016-10-27 14:32:46 +0200

    Teams enter code and subscribe to Quiz channel (#6)
    * Add code entry form to index page
    * Start writing findQuiz() function
    * Get quiz from server, use to subscribe to right channel
    * Get quiz into the view
    * Register team button goes to the right function
    * Send team name and quiz id to server
    * Hide team form when a team name has been entered
    * Get submitAnswer working again
    * Appropriately hide answer submission form
    * Error handle bad codes
    * Add ionic view deployment
    * Change attempted access server to equizmaster.com
    * Add  so teams know that the server is doing something
    * Auto-format code
    * Refactor controller to use .quiz

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,9 @@
-angular.module('quizmaster.controllers', [ 'ngActionCable' ])
+angular.module('quizmaster.controllers', ['ngActionCable'])
 
   .controller('DashCtrl', function ($scope) {
   })
 
-  .controller('QuizController', function ($scope, $ionicModal, ActionCableChannel, $interpolate) {
+  .controller('QuizController', function ($scope, $ionicModal, ActionCableChannel, $ionicLoading) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -11,58 +11,100 @@ angular.module('quizmaster.controllers', [ 'ngActionCable' ])
     //
     //$scope.$on('$ionicView.enter', function(e) {
     //});
+    var consumer, callback, quiz, team_name;
 
-    $scope.goToQuiz = function () {
-      $ionicModal.fromTemplateUrl('templates/quiz-page.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function (modal) {
-        $scope.modal = modal;
-        $scope.openModal();
-      });
-    };
+    $scope.findQuiz = function () {
+      $ionicLoading.show({
+          template: 'Loading quiz...'
+        }
+      );
+      code = angular.element(document.querySelector('#codeEntry'))[0].value;
+      consumer = new ActionCableChannel('QuizChannel');
+      callback = function (obj) {
+        if (obj != null) {
+          $scope.quiz = obj;
+          consumer.unsubscribe()
+            .then(function () {
+              subscribeToQuiz();
+              angular.element(document.querySelector('#badCode'))[0].style.display = "none";
+              $ionicModal.fromTemplateUrl('templates/quiz-page.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+              }).then(function (modal) {
+                $scope.modal = modal;
+                $ionicLoading.hide();
+                $scope.openModal();
+              });
+            });
+        } else {
+          $ionicLoading.hide();
+          angular.element(document.querySelector('#badCode'))[0].style.display = "inline";
+        }
 
-    var consumer = new ActionCableChannel('QuizChannel', {quiz_id: 43, team_id: 2});
-    var callback = function(data) {
-      console.log(data);
-      if(data.welcome == 'true'){
-        angular.element(document.querySelector('#message')).html(data.message);
-      } else {
-        angular.element(document.querySelector('#message')).html(data);
-      }
-    };
-
-    $scope.openModal = function () {
-      $scope.modal.show();
+      };
       consumer.subscribe(callback)
-        .then(function() {
-          this.submitAnswer = function() {
-            var dataset, quiz, answer, question, team, answer_hash ;
+        .then(function () {
+          consumer.send(code, 'get_quiz');
+        });
+
+
+    };
+
+    var subscribeToQuiz = function () {
+      callback = function (data) {
+        if (data.welcome == 'true') {
+          angular.element(document.querySelector('#message')).html(data.message);
+        } else {
+          angular.element(document.querySelector('#message')).html(data);
+          angular.element(document.querySelector('.answer_submitted'))[0].style.display = "none";
+        }
+      };
+      consumer = new ActionCableChannel('QuizChannel', {quiz_id: $scope.quiz.id});
+      consumer.subscribe(callback)
+        .then(function () {
+          $scope.registerTeam = function () {
+            team_name = angular.element(document.querySelector('#teamName'))[0].value;
+            if (team_name.trim().length >= 1) {
+              $scope.team_name = team_name;
+              message = {team_name: team_name, quiz_id: $scope.quiz.id};
+              consumer.send(message, 'create_team');
+              $scope.registeredTeam = true;
+              angular.element(document.querySelector('#message')).html('Questions will appear here. Hang tight.');
+            } else {
+              angular.element(document.querySelector('#message')).html('Enter a team name!');
+            }
+
+          };
+          this.submitAnswer = function () {
+            var dataset, quiz, answer, question, team, answer_hash;
             dataset = angular.element(document.querySelector('#info'))[0];
             quiz = dataset.getAttribute('data-quiz-id');
-            // team = dataset.getAttribute('data-team-id');
-            team = 2;
+            team = team_name;
             question = dataset.getAttribute('data-question-id');
             answer = angular.element(document.querySelector('#body'))[0].value;
 
-            var sendAnswer = function() {
+            var sendAnswer = function () {
               consumer.send(answer_hash, 'submit_answer');
             };
 
             if (answer.trim().length >= 1) {
-              // Team ID needs to come from cookies or something here. - hard-coded as 1 for the moment
-              answer_hash = {answer: answer, team_id: team, quiz_id: quiz, question_id: question};
+              answer_hash = {answer: answer, team_name: team, quiz_id: quiz, question_id: question};
               sendAnswer(answer_hash);
-              // $('.answer_form').hide();
-              // $('.wait').show();
+              angular.element(document.querySelector('.answer_submitted'))[0].style.display = "inline";
+              angular.element(document.querySelector('.answer_form'))[0].style.display = "none";
+
             } else {
               angular.element(document.querySelector('#message')).html('Enter an answer!');
             }
             return false;
           };
         });
+    };
 
+    $scope.openModal = function () {
+      $scope.modal.show();
     }
 
-  });
+  })
+;
 
